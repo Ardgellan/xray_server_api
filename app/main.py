5f41e3feb101b22b08e0ac3250e55449fb7e0928
Ardgellan <aegiev01@gmail.com>
2024-11-18 04:42:43 +0300

    start_39

--- app/main.py
+++ app/main.py
@@ -8,40 +8,32 @@ from app.xray import xray_config
 
 app = FastAPI()
 
-# Данные для каждого сервера по стране
-server_data: Dict[str, Dict[str, str]] = {
-    "estonia": {"message": "User added successfully from Estonia!", "servers": ["estoniatest1", "estoniatest2"]},
-    "germany": {"message": "User added successfully from Germany!", "servers": ["germanytest"]},
-    "japan": {"message": "User added successfully from Japan!", "servers": ["japantest"]}
-}
 
 logger.add("logs/app_{time}.log", rotation="10 MB", compression="zip")
 
 @app.post("/add_user/{country}/")
 async def add_user(country: str, user_id: int, config_name: str):
-    logger.info(f"Received request for {country} with user_id={user_id} and config_name={config_name}, Salam")
-    if country in server_data:
-        try:
-            # Вызов функции для добавления нового пользователя
-            user_link, config_uuid = await xray_config.add_new_user(config_name=config_name, user_telegram_id=user_id)
-            server_domain = config.domain_name
-            server_country = config.server_country  # Название страны
-            server_country_code = config.server_country_code  # Код страны
+    logger.info(f"Received request for {country} with user_id={user_id} and config_name={config_name}")
+    
+    try:
+        # Вызов функции для добавления нового пользователя
+        user_link, config_uuid = await xray_config.add_new_user(config_name=config_name, user_telegram_id=user_id)
+        server_domain = config.domain_name
+        server_country = config.server_country  # Название страны
+        server_country_code = config.server_country_code  # Код страны
 
-            # Передаем данные в ответе
-            return {
-                "message": server_data[country]["message"],
-                "link": user_link,
-                "config_uuid": config_uuid,  # UUID передаем в ответ
-                "server_domain": server_domain,
-                "server_country": server_country,  # Отправляем название страны
-                "server_country_code": server_country_code  # Отправляем код страны
-            }
+        # Передаем данные в ответе
+        return {
+            "link": user_link,
+            "config_uuid": config_uuid,  # UUID передаем в ответ
+            "server_domain": server_domain,
+            "server_country": server_country,  # Отправляем название страны
+            "server_country_code": server_country_code  # Отправляем код страны
+        }
 
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Failed to add user: {str(e)}")
-    else:
-        raise HTTPException(status_code=404, detail="Country not supported")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to add user: {str(e)}")
+
 
 
 @app.post("/reactivate_configs/{target_server}/")
@@ -82,13 +74,6 @@ async def show_specified_config(target_server: str, config_uuid: str, config_nam
     except Exception as e:
         logger.error(f"Ошибка при создании ссылки конфигурации: {str(e)}")
         raise HTTPException(status_code=500, detail="Не удалось создать ссылку конфигурации")
-
-
-# # Упрощенный эндпоинт для удаления пользователя
-# @app.delete("/delete_user/{target_server}/")
-# def delete_user(target_server: str):
-#     # Простое подтверждение удаления без привязки к стране
-#     return {"message": f"User deleted successfully from {target_server}!"}
 
 
 @app.delete("/delete_config/{target_server}/")
